1fcb414d8caa15cac96fbab89e4a7bd6f1eae087
Eric Kida <51220926+EricKida@users.noreply.github.com>
2022-09-05 16:03:42 -0300

    Aula 05/09
    Servlets com entrada de parâmetros

---
+++ 05-09/Ex02/src/java/web/RandomServlet.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
+/**
+ *
+ * @author Fatec
+ */
+@WebServlet(name = "RandomServlet", urlPatterns = {"/RandomServlet.html"})
+public class RandomServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>MathServlet - RandomServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h4><a href='index.html'>Voltar</a></h4>");
+            int n3 = 0;
+            try{
+                n3 = Integer.parseInt(request.getParameter("n3"));
+            }catch(Exception ex){
+                out.println("<div style='color:red;'> - Parâmetro Inválido</div>");
+            }
+            
+            out.println(
+                "<form>" +
+                    "Digite quantos números aleatórios você deseja: <input type='number' step='0.1' name='n3' value='"+n3+"'>" +
+                    "<input type='submit' name='somar' value='Calcular'><br><br>" +
+                "</form>"
+            );
+            out.println("<table>");
+            Random gerar = new Random();
+           for (int i = 0; i < n3; i++) {
+           
+            out.println("<tr>");
+            out.println("<th>" + (i+1) + "</th>");
+            out.println("<td>" + gerar.nextInt(100) + "</td>");
+            out.println("</tr>");
+
+         }
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
